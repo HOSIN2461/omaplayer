d5960b656294514176eceec37119873e27690b73
Molnár Péter <urage2461@gmail.com>
2026-09-05 01:15:51 +0200

    Hungarian UI translation and v0.1.0 packaging
    - Fill hu.ts with Hungarian translations for all 21 UI strings and ship
      the pre-compiled hu.qm (embedded via :/translations)
    - Add PKGBUILD + omaplayer.install on the omafiles pattern

--- translations/hu.ts
+++ translations/hu.ts
@@ -1,10 +1,115 @@
 <?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
 <TS version="2.1" language="hu_HU">
-  <context>
+<context>
+    <name>ControlBar</name>
+    <message>
+        <location filename="../qml/ControlBar.qml" line="63"/>
+        <source>Pause</source>
+        <translation>Szünet</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlBar.qml" line="63"/>
+        <source>Play</source>
+        <translation>Lejátszás</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlBar.qml" line="70"/>
+        <source>Stop</source>
+        <translation>Leállítás</translation>
+    </message>
+    <message>
+        <location filename="../qml/ControlBar.qml" line="189"/>
+        <source>Mute</source>
+        <translation>Némítás</translation>
+    </message>
+</context>
+<context>
     <name>Main</name>
     <message>
-      <source>Omaplayer</source>
-      <translation>Omaplayer</translation>
+        <location filename="../qml/Main.qml" line="63"/>
+        <source>Omaplayer</source>
+        <translation>Omaplayer</translation>
     </message>
-  </context>
+    <message>
+        <location filename="../qml/Main.qml" line="139"/>
+        <source>Open media…</source>
+        <translation>Megnyitás…</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="143"/>
+        <source>Open URL…</source>
+        <translation>URL megnyitása…</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="147"/>
+        <source>Screenshot</source>
+        <translation>Képernyőkép</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="151"/>
+        <source>Exit PiP</source>
+        <translation>Kilépés a kis képből</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="151"/>
+        <source>Picture-in-Picture</source>
+        <translation>Kis kép a képen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="156"/>
+        <source>Pause</source>
+        <translation>Szünet</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="156"/>
+        <source>Play</source>
+        <translation>Lejátszás</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="160"/>
+        <source>Exit fullscreen</source>
+        <translation>Kilépés a teljes képernyőből</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="160"/>
+        <source>Fullscreen</source>
+        <translation>Teljes képernyő</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="205"/>
+        <source>Open media</source>
+        <translation>Fájl megnyitása</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="207"/>
+        <source>Media files (%1)</source>
+        <translation>Médiafájlok (%1)</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="208"/>
+        <source>All files (*)</source>
+        <translation>Minden fájl (*)</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="215"/>
+        <source>Open URL</source>
+        <translation>URL megnyitása</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="222"/>
+        <source>https://…</source>
+        <translation>https://…</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="303"/>
+        <source>Omaplayer — kis kép a képen</source>
+        <translation>Omaplayer — kis kép a képen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Main.qml" line="311"/>
+        <source>Bezárás (PiP kilépés)</source>
+        <translation>Bezárás (PiP kilépés)</translation>
+    </message>
+</context>
 </TS>
